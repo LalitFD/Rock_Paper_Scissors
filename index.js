8e1391da5e81b50864262f944fb5414c69392e98
Lalit doriya <158010530+LalitFD@users.noreply.github.com>
2024-02-17 12:30:21 +0530

    Rock Paper Scissors game
    I Have practice for the Rock Paper Scissors  Game

---
+++ index.js
@@ -0,0 +1,51 @@
+let userScore = 0;
+let compScore = 0;
+
+const choices = document.querySelectorAll(".choice");
+const msg = document.getElementById("msg");
+
+const genCompChoice = () => {
+    const options = ["rock", "paper", "scissor"];
+    const randIdx = Math.floor(Math.random() * 3);
+    return options[randIdx];
+};
+
+const drawGame = () => {
+    msg.innerText = "The game was a draw!";
+};
+
+const showWinner = (userWin) => {
+    if (userWin) {
+        msg.innerText = "You win!";
+    } else {
+        msg.innerText = "You lose!";
+    }
+};
+
+const playGame = (userChoice) => {
+    const compChoice = genCompChoice();
+
+    if (userChoice === compChoice) {
+        drawGame();
+    } else {
+        let userWin = true;
+
+        if (userChoice === "rock") {
+            userWin = compChoice === "paper" ? false : true;
+        } else if (userChoice === "paper") {
+            userWin = compChoice === "scissor" ? false : true;
+        } else {
+            userWin = compChoice === "rock" ? false : true;
+        }
+
+        showWinner(userWin);
+    }
+};
+
+choices.forEach((choice) => {
+    choice.addEventListener("click", () => {
+        const userChoice = choice.getAttribute("id");
+        console.log("Choice was clicked", userChoice);
+        playGame(userChoice);
+    });
+});
